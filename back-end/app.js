4c17431db764df80d61b6b32d66cbf99c335bfb1
unknown <20220224@ricaldone.edu.sv>
2025-06-13 15:44:19 -0600

    implementacion de swagger

--- back-end/app.js
+++ back-end/app.js
@@ -13,24 +13,34 @@ import registerClient from "./src/routes/registerClient.js";
 import RecoveryPassword from "./src/routes/RecoveryPassword.js";
 import providersRoutes from "./src/routes/providers.js";
 import { validateAuthToken } from "./src/middlewares/validateAuthToken.js";
-import faqsRoutes from "./src/routes/faqs.js"
-import cors from "cors"
+import faqsRoutes from "./src/routes/faqs.js";
+import cors from "cors";
+import swagger from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 // Crea una constante que es igual a la librería que importe
 const app = express();
 
 //middleware para aceptar datos desde postman
 app.use(express.json());
 app.use(cookieParser());
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./DOCUMENTACION_TALEGONA.json"), "utf-8")
+);
 
-app.use(cors({
-     origin: "https://api-productos-js.vercel.app",
-      credentials: true }));
+app.use(
+  cors({
+    origin: "https://api-productos-js.vercel.app",
+    credentials: true,
+  })
+);
 //Vinculando la url a la ruta
 app.use(
   "/api/products",
   // validateAuthToken(["employee", "admin"]),
   productsRoutes
 );
+app.use("/api/docs", swagger.serve, swagger.setup(swaggerDocument));
 app.use("/api/clients", clientsRoutes);
 app.use("/api/employees", employeesRoutes);
 app.use(
@@ -48,7 +58,7 @@ app.use("/api/login", login);
 app.use("/api/logout", logout);
 app.use("/api/registerClient", registerClient);
 app.use("/api/RecoveryPassword", RecoveryPassword);
-app.use("/api/faqs",faqsRoutes)
+app.use("/api/faqs", faqsRoutes);
 app.use(
   "/api/providers",
   validateAuthToken(["employee", "admin"]),
